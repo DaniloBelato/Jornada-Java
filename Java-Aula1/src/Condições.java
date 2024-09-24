2e9fac81b9b26634cfd312712e8d2487722cd0d8
Danilo Belato <danilo.belato@alunos.unis.edu.br>
2024-09-24 20:45:24 -0300

    novo commit

---
+++ Java-Aula1/src/Condições.java
@@ -0,0 +1,23 @@
+public class Condições {
+
+    public static void main(String[] args) throws Exception {
+        boolean cruzeiro = true;
+
+        boolean atletico = false;
+
+        if (cruzeiro && atletico) {
+            System.out.println("Cruzeiro e Atletico são Grandes");
+        } else if (cruzeiro || atletico) {
+            System.out.println("Cruzeiro é Grande!!");
+        } else {
+            System.out.println("Atletico e grande");
+        }
+}
+}
+
+/* 
+ * Operadores lógicos
+ * && = AND
+ * || = OR
+ * ! = NOT
+ */
