a57c2405260e5b71ae05e06a1d6776d2a8defac7
Danilo Belato <danilo.belato@alunos.unis.edu.br>
2024-09-24 19:51:29 -0300

    novo commit

--- Java-Aula1/src/TiposVariaveis.java
+++ Java-Aula1/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("teste de variáveis");
+
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
     }
 }
